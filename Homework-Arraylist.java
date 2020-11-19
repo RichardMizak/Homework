9550df00612a0a8de854d0b59c257bc61493ff4f
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2020-11-19 22:06:18 +0100

    Add files via upload

---
+++ Homework-Arraylist.java
@@ -0,0 +1,28 @@
+public class Arraylist {
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        ArrayList<String> arr = new ArrayList<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\names.txt")))
+        {
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                arr.add(sCurrentLine);
+
+            }
+            ArrayList<Integer> newList = removeDuplicates(arr);
+            System.out.println(newList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static <T> ArrayList<T> removeDuplicates(ArrayList<String> list)
+    {
+        ArrayList<T> newList = new ArrayList<T>();
+        for (String element : list) {
+            if (!newList.contains(element)) {
+                newList.add((T) element);
+            }
+        }
+        return newList;
+    }
+}
