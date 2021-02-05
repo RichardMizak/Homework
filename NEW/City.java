b3f3864e26d903da7dadbd988c0a6ae4186ba638
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-02-05 11:07:39 +0100

    Update City.java

--- NEW/City.java
+++ NEW/City.java
@@ -3,17 +3,41 @@ package sk.kosickaakademia.mysql;
 public class City {
     private String name;
     private int population;
+    private String country;
+    private String code3;
+    private String district;
 
-    public City(String name,int population){
+    public City(String name,String country, String code3, String district, int population){
     this.name=name;
     this.population=population;
+        this.country = country;
+        this.code3 = code3;
+        this.district = district;
+
+
     }
 
     public String getName() {
+        
         return name;
     }
 
     public int getPopulation() {
         return population;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCode3(String code3) {
+    }
+
+    public String getCode3() {
+        return code3;
+    }
+
+    public String getDistrict() {
+        return district;
     }
 }
