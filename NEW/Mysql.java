f4f47e645447593ae6e8b6ef2d44734b060274b3
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-02-05 11:05:27 +0100

    Update Mysql.java

--- NEW/Mysql.java
+++ NEW/Mysql.java
@@ -28,8 +28,8 @@ try {
             String name=rs.getString("Name");
          // String code=rs.getString("CountryCode");
             int pop=rs.getInt("Population");
-            City city=new City(name,pop);
-            cityList.add(city);
+        //    City city=new City(name,pop);
+          //  cityList.add(city);
 
         }
         conn.close();
@@ -67,5 +67,111 @@ return cityList;
         }
 
         return countryInfo;
+    }
+    public String getCountryCode(String name) {
+        if (name == null || name.equalsIgnoreCase(""))
+            return null;
+        try {
+            Connection con = getConnection();
+            String query = "SELECT Code FROM country WHERE Name LIKE ?";
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, name);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                String code = rs.getString("Code");
+                return code;
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+        public void insertCity(City newCity) {
+
+            String country = newCity.getCountry();
+            String code3 = getCountryCode(country);
+            if(code3==null){
+                System.out.println("Warning! Country "+country+" does not exist! ");
+            }
+            else{
+                newCity.setCode3(code3);
+                String query = "INSERT INTO city (Name, CountryCode, District, Info) " +
+                        "VALUES( ?,?,?,?) ";
+                try {
+                    Connection con = getConnection();
+                    PreparedStatement ps = con.prepareStatement(query);
+                    ps.setString(1,newCity.getName());
+                    ps.setString(2,newCity.getCode3());
+                    ps.setString(3,newCity.getDistrict());
+                    String json="{ \"Population\":"+newCity.getPopulation()+"}";
+                    ps.setString(4,json);
+                    boolean result= ps.execute();
+                    System.out.println("Result: "+result);
+                    con.close();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    public void updatePopulation(String country, String city, int population){
+     //   String country = city.getCountry();
+        String code3 = getCountryCode(country);
+        if(code3==null){
+            System.out.println("Warning! Country "+country+" does not exist! ");
+        }
+        City newPopulation = null;
+        population=newPopulation.getPopulation();
+
+        String query="INSERT INTO city (Population) " +
+                     "VALUES( ?) ";
+
+        try {
+            Connection con=getConnection();
+            PreparedStatement ps=con.prepareStatement(query);
+            ps.setString(1, country);
+            ps.setString(2, city);
+            ps.setString(3, String.valueOf(population));
+           // String json="{\\\"Population\\\":"++"}";
+
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<CapitalCity> getCapitalCity(String continent){
+        String query="SELECT city.Name, AS Name ," +
+                "JSON_EXTRACT(Info,'$Population') AS Population "+
+                "FROM country "+
+                "INNER JOIN city ON country.Capital = city.ID "+
+                "INNER JOIN countryinfo ON country.code = countryinfo._id "+
+                "WHERE JSON_UNQUOTE(JSON_EXTRACT(doc, '$.geography.Continent')) like ?";
+        List<CapitalCity> cityList=new ArrayList<>();
+        try {
+            Connection conn = getConnection();
+            if(conn!=null){
+                System.out.println("Success");
+                PreparedStatement ps= conn.prepareStatement(query);
+                System.out.println(ps);
+                ps.setString(1,continent);
+                ResultSet rs= ps.executeQuery();
+                while(rs.next()){
+                    String name=rs.getString("Name");
+                    String country=rs.getString("country");
+                    int pop=rs.getInt("Population");
+
+                }
+                conn.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
